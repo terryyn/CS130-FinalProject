91865c7adcd2e4a984fd0268339898cc30753a7e
Alexander Xu <xualexander1@gmail.com>
2019-11-12 20:36:40 -0800

    Style sidebar

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,5 @@
-import React, {useState} from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-  Redirect,
-  useHistory,
-  useLocation
-} from "react-router-dom";
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link, Redirect, useHistory, useLocation } from 'react-router-dom';
 
 import Login from './containers/Login';
 import Meeting from './containers/Meeting';
@@ -18,26 +10,26 @@ import Sidebar from './components/Sidebar';
 import './App.css';
 
 function App() {
-  const {isLoggedIn, setLoggedIn} = useState(true);
+	const [ isLoggedIn, setLoggedIn ] = useState(true);
 
-  return (
-    <div className="App">
-      <Router>
-          <Sidebar/>
-          <Switch>
-            <Route exact path="/">
-              <Login/>
-            </Route>
-            <Route path="/home">
-              <Home/>
-            </Route>
-            <Route path="/meeting">
-              <Meeting/>
-            </Route>
-          </Switch>
-      </Router>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Router>
+				{isLoggedIn && <Sidebar />}
+				<Switch>
+					<Route exact path="/">
+						<Login />
+					</Route>
+					<Route path="/home">
+						<Home />
+					</Route>
+					<Route path="/meeting">
+						<Meeting />
+					</Route>
+				</Switch>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
--- app/src/components/Sidebar.js
+++ app/src/components/Sidebar.js
@@ -1,11 +1,58 @@
-import React, {useState} from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import { CalendarToday, ExitToApp, MeetingRoom } from '@material-ui/icons';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 
 function Sidebar() {
-    return <div>
-        <Link to="/home">Home</Link>
-        <Link to="/meeting">Meeting</Link>
-    </div>;
+	const useStyles = makeStyles({
+		list: {
+			width: 250
+		},
+		fullList: {
+			width: 'auto'
+		}
+	});
+
+	return (
+		<Drawer open variant="permanent" className="sidebar">
+			<div className={useStyles().list}>
+				<List>
+					<ListItem>
+						<ListItemIcon>
+							<CalendarToday />
+						</ListItemIcon>
+						<ListItemText>
+							<Link to="/home">Home</Link>
+						</ListItemText>
+					</ListItem>
+					<ListItem>
+						<ListItemIcon>
+							<MeetingRoom />
+						</ListItemIcon>
+						<ListItemText>
+							<Link to="/meeting">Meeting</Link>
+						</ListItemText>
+					</ListItem>
+					<Divider />
+					<ListItem>
+						<ListItemIcon>
+							<ExitToApp />
+						</ListItemIcon>
+						<ListItemText>
+							<Link to="/">Sign out</Link>
+						</ListItemText>
+					</ListItem>
+				</List>
+			</div>
+		</Drawer>
+	);
 }
 
 export default Sidebar;
