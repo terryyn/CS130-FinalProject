3487effac51d5c6b6d3615d1cf2915da0f4c4d00
Jingyue <Jingyue_shen@yahoo.com>
2019-11-12 16:56:06 -0800

    Changes database schema

--- UCal/model.py
+++ UCal/model.py
@@ -4,14 +4,15 @@ from . import db, login_manager
 class User(UserMixin, db.Model):
     '''
     table users
-    user_id | user_name | email | password | is_instructor
+    id | username | email | password_hash | is_instructor
     
     '''
     __table_name__ = 'users'
-    user_id = db.Column(db.Integer, primary_key=True)
-    user_name = db.Column(db.String(64), unique=True, index = True)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index = True)
     email = db.Column(db.String(128), unique=True, index = True)
     password_hash = db.Column(db.String(128))
+    participations = db.relationship('Participation', backref= 'user', lazy = 'dynamic')
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
@@ -27,19 +28,30 @@ class Event(db.Model):
     '''
     event table:
 
-    id  |  eventName  |  userID  |  date  |  time  |  location  |  description  |  eventType  |  participantsID
+    id  | eventName |  date  |  time  |  location  |  description  |  eventType
 
     '''
     __tablename__ = 'events'
     id = db.Column(db.Integer, primary_key=True)
     eventName = db.Column(db.String(64))
-    userID = db.Column(db.Integer)
+    # userID = db.Column(db.Integer, db.ForeignKey('users.id'))
     date = db.Column(db.Date)
     time = db.Column(db.Time)
     location = db.Column(db.String(64))
     description = db.Column(db.String(64), nullable=True)
     eventType = db.Column(db.Integer, default=EventType.DEFAULT)
-    participants = db.Column(db.PickleType, nullable=True)
+    participations = db.relationship('Participation', backref= 'event', lazy = 'dynamic')
+
+class Participation(db.Model):
+    '''
+    participation table
+    event_id | user_id 
+
+    '''
+    __tablename__ = 'participation'
+    event_id = db.Column(db.Integer, db.ForeignKey('events.id'), primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
+
 
 def add_event(event_json):
     new_event = Event(eventName=event_json[eventName], userID=event_json[userID], date=event_json[date], time=event_json[time], location=event_json[location], description=event_json[description], eventType=event_json[eventType], participants=event_json[participants])
