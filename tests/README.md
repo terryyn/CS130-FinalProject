9c8e9696beff0b48948c294a8f06a09b3a491804
Jinjing <jilltrinity1011@gmail.com>
2019-11-15 00:20:59 -0800

    added integration tests

--- tests/README.md
+++ tests/README.md
@@ -1,17 +1,19 @@
-This directory contains both unit tests and integration tests implemented for the back end.
-test_basics.py has the most basic test implemented.
-test_db_manager.py has unit tests for methods in the DatabaseManager Class.
-test_db_manager_integration.py has integration tests to test if methods in the DatabaseManager Class are interacting as expected with the database.
-test_find_study_room.py has integration tests to test the interaction with external resouces when searching for a study room.
+This directory contains both unit tests and integration tests implemented for the back end.  
+test_basics.py has the most basic test implemented.  
+test_db_manager.py has unit tests for methods in the DatabaseManager Class.  
+test_db_manager_integration.py has integration tests to test if methods in the DatabaseManager Class are interacting as expected with the database.  
+test_find_study_room.py has integration tests to test the interaction with external resouces when searching for a study room.  
+  
 
+  
+The unittests are to be run in the main directory using the following commands:  
+To run tests in one test file:  
+    python3 -m unittest tests.test_db_manager  
+To run tests in multiple test files:  
+    python3 -m unittest tests.test_db_manager tests.test_db_manager_integration  
+To run a particular test in a test file  
+    python3 -m unittest tests.test_db_manager.test_add_event  
+  
 
-The unittests are to be run in the main directory using the following commands:
-To run tests in one test file:
-    python3 -m unittest tests.test_db_manager
-To run tests in multiple test files:
-    python3 -m unittest tests.test_db_manager tests.test_db_manager_integration
-To run a particular test in a test file
-    python3 -m unittest tests.test_db_manager.test_add_event
-
-
-Some of the code in this is adapted from the book Flask Web Development-Developing Web Applications with Python [Grinberg 2014-05-18]
+Some of the test codes are adapted from the book Flask Web Development_ Developing Web Applications with Python [Grinberg 2014-05-18].pdf
+  
--- tests/test_db_manager_integration.py
+++ tests/test_db_manager_integration.py
@@ -1,6 +1,78 @@
-from datetime import date, time
+import datetime
 import unittest
 from unittest.mock import Mock, patch
 import json
 from UCal import create_app, db, db_manager
+<<<<<<< HEAD
 from UCal.model import User, Event, EventType, Participation
+
+def get_meeting_json():
+    return {
+        'meeting_name' : 'CS130 Discussion', 
+        'participants' : ['Joe Bruin','Josephine Bruin'], 
+        'possible_days': [datetime.datetime(2019,12,10),datetime.datetime(2019,12,13)],
+        'meet_duration': 60,
+        'earliest_meet_time': datetime.datetime(hour=9,minute=30),
+        'latest_meet_time': datetime.datetime(hour=19,minute=0)
+    }
+
+def get_user1_info():
+    return {
+        'username': 'Joe Bruin',
+        'email' : 'joebruin@ucal.com',
+        'password' : 'password',
+        'is_instructor' : False
+    }
+
+def get_user2_info():
+    return {
+        'username': 'Josephine Bruin',
+        'email' : 'josbruin@ucal.com',
+        'password' : 'drowssap',
+        'is_instructor' : False
+    }
+
+def get_event_info():
+    return {
+        'name' : 'Joe Bruin',
+        'startdate' : '2019-12-13',
+        'starttime' : '11:30',
+        'enddate' : '2019-12-13',
+        'endtime' : '14:30',
+        'location' : 'Boelter 3400',
+        'type' : EventType.EXAM,
+        'description' : 'This is the final exam for CS130 Fall 2019'
+    }
+
+class DBManagerIntegrationTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app('test')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+    
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+    
+    def testAddUserLogin(self):
+        user1_info = get_user1_info()
+        user2_info = get_user2_info()
+        addSuccess1 = db_manager.add_user(user1_info)
+        addSuccess2 = db_manager.add_user(user2_info)
+        assert addSuccess1 and addSuccess2
+        user1 = db_manager.log_in(user1_info)
+        assert user1 != None
+        user2_info['password'] = 'password'
+        user2 = db_manager.log_in(user2_info)
+        assert user2 == None
+
+    def testAddEditDeleteEvent(self):
+        user1_info = get_user1_info()
+        user2_info = get_user2_info()
+        db_manager.add_user(user1_info)
+        db_manager.add_user(user2_info)
+        event1_info = get_event_info()
+        event_id = db_manager.add_event_to_database(event1_info)
+        assert event_id != None
