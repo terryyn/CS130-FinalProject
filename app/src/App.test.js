2442523ca1137b4133a7c8a89466e7942abf6130
Terry Ye <yeyunong@hotmail.com>
2019-11-29 22:17:35 -0800

    add the end to end test of sign up

--- app/src/App.test.js
+++ app/src/App.test.js
@@ -14,19 +14,46 @@ describe('App', () =>{
     expect(tree).toMatchSnapshot();
   });
 
-  test('mock log in', async () => {
+  test('mock sign up', async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('http://localhost:3000/');
+
+    //make sure the log in page popos up
     await page.waitForSelector('#regLogin');
 
     // check nonexistent account
+    // const signup = await page.$x("//Link[contains(text(),'Don\'t have an account? Sign Up')]");
+    // if (signup) {
+    //     await signup.click();
+    // }
+    await clickByText(page,"Don\'t have an account? Sign Up");
+
+    await page.click('input[name=name');
+    await page.type('input[name=name]','')
     await page.click('input[name=email]');
     await page.type('input[name=email]','yeyunong@hotmail.com');
     await page.click('input[name=password]');
     await page.type('input[name=password]','12345678');
-    await page.click('Button');
+    await page.keyboard.press('Enter');
 
-    await page.$eval("Snackbar", el => el.value = "Looks like you don't have an account. Let's get you signed up7");
+
+    await page.$eval("#message-id", el => el.value = 'Invalid email. Must have a g.ucla.edu email');
   });
 });
+
+const clickByText = async (page, text) => {
+  const escapedText = escapeXpathString(text);
+  const linkHandlers = await page.$x(`//a[contains(text(), ${escapedText})]`);
+  
+  if (linkHandlers.length > 0) {
+    await linkHandlers[0].click();
+  } else {
+    throw new Error(`Link not found: ${text}`);
+  }
+};
+
+const escapeXpathString = str => {
+  const splitedQuotes = str.replace(/'/g, `', "'", '`);
+  return `concat('${splitedQuotes}', '')`;
+};
